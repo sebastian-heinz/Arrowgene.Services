4627d684130c26a7af86f125112b347139f35a83
sebastian.heinz.gt <sebastian.heinz.gt@googlemail.com>
2015-02-02 19:14:39 +0100

    restructured folder layout

---
+++ RemoteControlDemo/ConsoleClient/Packets/SendScreenshot.cs
@@ -0,0 +1,68 @@
+﻿namespace ConsoleClient.Packets
+{
+    using MarrySocket.MClient;
+    using NetworkObjects;
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.IO;
+    using System.Windows.Forms;
+
+    public class SendScreenShot : ISendPacket
+    {
+        private long quality;
+
+        public SendScreenShot(long quality)
+        {
+            this.quality = quality;
+        }
+
+        public void Send(ServerSocket serverSocket)
+        {
+            byte[] screen = null;
+
+            int screenLeft = SystemInformation.VirtualScreen.Left;
+            int screenTop = SystemInformation.VirtualScreen.Top;
+            int screenWidth = SystemInformation.VirtualScreen.Width;
+            int screenHeight = SystemInformation.VirtualScreen.Height;
+
+            ImageCodecInfo jgpEncoder = GetEncoder(ImageFormat.Jpeg);
+            Encoder myEncoder = Encoder.Quality;
+            EncoderParameters myEncoderParameters = new EncoderParameters(1);
+            EncoderParameter myEncoderParameter = new EncoderParameter(myEncoder, this.quality);
+            myEncoderParameters.Param[0] = myEncoderParameter;
+
+            using (Bitmap bmp = new Bitmap(screenWidth, screenHeight))
+            {
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    g.CopyFromScreen(screenLeft, screenTop, 0, 0, bmp.Size);
+                }
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    bmp.Save(ms, jgpEncoder, myEncoderParameters);
+                    screen = ms.ToArray();
+                }
+            }
+
+            ScreenShot screenShot = new ScreenShot(screen);
+
+            serverSocket.SendObject(0, screenShot);
+        }
+
+        private ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+    }
+}
