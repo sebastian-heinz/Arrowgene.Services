ed72c4e0a7a0f60260c37ff6e55f3c98d8727bff
sebastian heinz <sebastian.heinz.gt@googlemail.com>
2015-07-14 16:43:37 +0200

    added components

---
+++ SvrKit/Kits/AppKit.cs
@@ -0,0 +1,49 @@
+﻿namespace SvrKit.Kits
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Reflection;
+
+    public static class AppKit
+    {
+
+        public static string DirectoryPath
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().EscapedCodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
+
+        public static void TryCreateDirectory(string path)
+        {
+            try
+            {
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("AppKit::TryCreateDirectory:" + ex.ToString());
+            }
+        }
+
+        public static string Version
+        {
+            get
+            {
+                System.Reflection.Assembly current = System.Reflection.Assembly.GetExecutingAssembly();
+                string exe = current.GetModules()[0].FullyQualifiedName;
+                FileVersionInfo fi = FileVersionInfo.GetVersionInfo(exe);
+                return fi.FileVersion;
+            }
+        }
+
+    }
+}
---
+++ SvrKitTest/Networking/MarrySocket/MarrySocketConnection.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SvrKit.Networking.MarrySocket.MClient;
+using SvrKit.Networking.MarrySocket.MServer;
+using System.Threading;
+
+namespace SvrKitTest
+{
+    [TestClass]
+    public class MarrySocketConnection
+    {
+        private MarryServer server;
+        private MarryClient client;
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.server = new MarryServer(new ServerConfig());
+            this.client = new MarryClient(new ClientConfig());
+        }
+
+
+        [TestMethod]
+        public void Connectivity()
+        {
+            this.Connect();
+
+            Assert.IsTrue(server.IsListening);
+            Assert.IsTrue(client.IsConnected);
+
+            this.Disconnect();
+
+            Assert.IsFalse(server.IsListening);
+            Assert.IsFalse(client.IsConnected);
+        }
+
+        [TestMethod]
+        public void Sending()
+        {
+            int myId = 1000;
+            string myMsg = "hello";
+            bool wait = true;
+            string clientReceived = null; ;
+            string serverReceived = null; ;
+
+            this.server.ReceivedPacket += (sender, e) =>
+            {
+                serverReceived = e.MyObject as string;
+                e.ClientSocket.SendObject(myId, myMsg);
+            };
+
+            this.client.ReceivedPacket += (sender, e) =>
+            {
+                clientReceived = e.MyObject as string;
+                wait = false;
+            };
+
+            this.Connect();
+            this.client.ServerSocket.SendObject(myId, myMsg);
+
+            while (wait)
+            {
+                Thread.Sleep(10);
+            }
+
+            Assert.IsTrue(clientReceived == myMsg);
+            Assert.IsTrue(serverReceived == myMsg);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            this.Disconnect();
+        }
+
+        private void Connect()
+        {
+            this.server.Start();
+            this.client.Connect();
+        }
+
+        private void Disconnect()
+        {
+            this.client.Disconnect();
+            this.server.Stop();
+        }
+
+    }
+}
