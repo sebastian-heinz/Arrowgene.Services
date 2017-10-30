6bb81b027cefab63d0e8408c9ad1163b5dedf791
sebastian-heinz <sebastian.heinz.gt@googlemail.com>
2017-10-30 22:53:10 +0800

    update

---
+++ publish.sh
@@ -0,0 +1,8 @@
+API_KEY=""
+VERSION="1.2.2"
+
+# https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-pack?tabs=netcore2x
+dotnet pack Arrowgene.Services/Arrowgene.Services.csproj --output ../nupkgs /p:Version=$VERSION
+
+# https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-nuget-push
+dotnet nuget push nupkgs/Arrowgene.Services.$VERSION.nupkg --api-key $API_KEY --source https://www.nuget.org/api/v2/package
