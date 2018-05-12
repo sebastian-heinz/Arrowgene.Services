e13b78bcf2f08fe112f36b406b6429501b52f4b4
sebastian-heinz <sebastian.heinz.gt@googlemail.com>
2018-05-12 13:15:09 +0800

    'error'

--- publish.cmd
+++ publish.cmd
@@ -1,4 +1,4 @@
-SET VERSION=1.91
+SET VERSION=1.9.1
 SET KEY=
 dotnet pack Arrowgene.Services/Arrowgene.Services.csproj --output ../nupkgs /p:Version=%VERSION%
 dotnet nuget push nupkgs/Arrowgene.Services.%VERSION%.nupkg --api-key %KEY% --source https://www.nuget.org/api/v2/package
